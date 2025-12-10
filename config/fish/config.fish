4f1eb03fe6a4eaa6c72f8d6b8eab0fd796e09c72
isaiah <ijshurtliff@gmail.com>
2025-12-10 20:46:09 +0000

    fixed fish file v2

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-    fastfetch
+    neofetch
 end
+
+# Created by `pipx` on 2025-12-10 20:18:13
+set PATH $PATH /home/dfir/.local/bin
